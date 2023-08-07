5a4e4cd77a5e478c9ad0f272481b71bf1372c3ab
devdrqgon <ahmed@valuecase.de>
2023-08-08 00:08:28 +0200

    start App with an empty state

--- src/components/hooks/useFunnel.tsx
+++ src/components/hooks/useFunnel.tsx
@@ -1,12 +1,11 @@
 import { create } from "zustand";
-import funnel from "./../../temp-input.json";
 interface AppState {
   funnel: Funnel | null;
   setFunnel: (funnel: Funnel) => void;
 }
 
 export const useFunnel = create<AppState>((set) => ({
-  funnel: funnel as Funnel,
+  funnel: null,
   setFunnel: (funnel: Funnel) => {
     set(() => ({
       funnel,
